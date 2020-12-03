c82971fb0449894efc559707677f5f27d2ca42b1
Alfonso <gomttang1@gmail.com>
2020-12-03 12:05:28 +0900

    web 추가

---
+++ web/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          카페노노 테스트 페이지.
+        </p>
+        <button>mobx 데이터 저장</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
